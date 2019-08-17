11da97f240d31cd5a1c17c2d9ba4a42fdcb1320a
Jose Nicdao <jonicdao@gmail.com>
2019-08-17 01:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,9 @@ const facts = [
   "He hated the sound of his own voice"
 ];
 function johnLennonFacts(facts) {
-  while 
+  let i = 0;
+  while (i < facts.length) {
+    return facts[i]
+    i++
+  }
 }
